b2f3fc7f39fcf4567941080a2d6a3808d037241c
[dyad] <tduong297@gmail.com>
2026-01-03 20:27:46 +0700

    [dyad] Install react-hot-toast - wrote 4 file(s), added react-hot-toast package(s)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,7 +2,13 @@
 
 import { ReactNode } from 'react';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { Toaster } from 'react-hot-toast';
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <Toaster position="top-center" />
+      {children}
+    </AuthProvider>
+  );
 }
